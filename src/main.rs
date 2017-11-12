7f815ed7f86cbead0e002511c0eaee463b1cbe92
Evan Seaward <seaward.evan@gmail.com>
2017-11-12 18:07:21 +1000

    Get root program directory contents

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use std::str;
 use std::path::PathBuf;
 use std::env;
 use std::vec::Vec;
+
 #[derive(Serialize, Deserialize)]
 struct User {
 	username: String,
@@ -20,31 +21,39 @@ struct User {
     paths: Vec<String>
 }
 
+#[derive(Serialize, Deserialize)]
+struct Directory {
+    dir_name: String,
+    dir_entries: Vec<String>
+}
+
 #[get("/")]
 fn index() -> &'static str {
     "Return index.html"
 }
 
-#[post("/showdir", format = "application/json", data = "<user>")]
-fn showdir(user: Json<User>) -> Json<User> {
+#[get("/showdir")]
+fn get_dir() -> Json<Directory> {
    
-    let mut username = user;
+    let dir_name = String::from(util::curr_dir().to_str().unwrap());
+    let mut entries: Vec<String> = Vec::new();
+
     let dir = util::fs::read::read_directory(&util::curr_dir());
     for entry in dir {
         match entry {
             Ok (e) => {
                 let path = &e.path();
                 let file_name = path.file_name().unwrap().to_str().unwrap();
-                username.paths.push(String::from(file_name));
+                entries.push(String::from(file_name));
             },
             Err(why) => {
                 println!("! {:?}", why.kind());
             },
         }
     }
-    return username;
+    return Json(Directory { dir_name: dir_name, dir_entries: entries});
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index, showdir]).launch();
+    rocket::ignite().mount("/", routes![index, get_dir]).launch();
 }
