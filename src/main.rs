4a2d25b3cd82c8db2bf30889a0b1153fa22f9747
Evan Seaward <seaward.evan@gmail.com>
2017-11-12 15:53:59 +1000

    login post test

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,21 @@
 
 extern crate rocket;
 
+struct User {
+	username: string,
+	password: string,
+}
+
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "Return index.html"
+}
+
+#[post("/login", format = "application/json", data = "<login>")]
+fn login(login: Json<User>) -> T {
+    "HeyYouJustTriedToLogin"
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, login]).launch();
 }
